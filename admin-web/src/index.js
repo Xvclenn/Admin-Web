ec537765a944a22474ce39b99599d33102ddd9a6
Xvclenn <benhhslen@gmail.com>
2023-01-05 00:43:14 +0800

    finished

--- admin-web/src/index.js
+++ admin-web/src/index.js
@@ -4,12 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Header } from "./components/header";
+import { Body } from "./components/body";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div className="nav">
+    <div className="NAV">
       <Header />
+    </div>
+    <div className="Bodd">
+      <Body />
     </div>
     <App />
   </React.StrictMode>
